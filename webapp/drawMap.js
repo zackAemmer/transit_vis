9bbc0a9c9406c4364ab499ec11b0a390d43e9a9c
zackAemmer <zae5op@gmail.com>
2021-01-27 10:40:06 -0800

    Basic coloring and geoJSON

---
+++ webapp/drawMap.js
@@ -0,0 +1,30 @@
+var map = L.map('map').setView([47.606209, -122.332069], 11);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([47.606209, -122.332069]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+var styleFunction = function(feature) {
+  if (feature.properties.TRANCLASS == 3) {
+    return {
+      "color": "#ff7800",
+      "weight": 5,
+      "opacity": 0.65
+    };
+  } else {
+    return {
+      "color": "#0000ff",
+      "weight": 5,
+      "opacity": 0.65
+    };
+  }
+};
+
+L.geoJSON(streets, {
+  style: styleFunction
+}
+).addTo(map);
