3990c206eaebb152294c2c92b692bb0f3f1d9b33
zackAemmer <zae5op@gmail.com>
2021-01-26 17:51:32 -0800

    Basic functioning map

--- webapp/script.js
+++ webapp/script.js
@@ -1,15 +1,26 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+
+/*
 let data = {};
 const dataUrl = 'https://cors-anywhere.herokuapp.com/https://docs.mapbox.com/help/data/stations.geojson';
 
 const map = L.map('mapid').setView([38.8977, -77.0365], 15);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   maxZoom: 18,
   id: 'mapbox/streets-v11',
   tileSize: 512,
   zoomOffset: -1,
-  accessToken: 'pk.eyJ1Ijoic3VkaXB0b2c4MSIsImEiOiJjanBzNXY1ZHUwNWI1NDNscHEzM3N3bnplIn0.ocXoObxyCMNmhzBOeRdQsA'
 }).addTo(map);
 
 fetch(dataUrl)
@@ -39,3 +50,4 @@ fetch(dataUrl)
     }).addTo(map);
   })
   .catch(err => console.error(err));
+  */
