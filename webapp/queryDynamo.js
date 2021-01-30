61176a42f548ea68d923193e73a1b57cfb7c1566
zackAemmer <zae5op@gmail.com>
2021-01-29 16:23:19 -0800

    AWS Dynamo connection

--- webapp/queryDynamo.js
+++ webapp/queryDynamo.js
@@ -1,40 +1,42 @@
+var REGION = process.env.REGION;
+var ACCESS_ID = process.env.ACCESS_ID;
+var ACCESS_KEY = process.env.ACCESS_KEY;
+
 AWS.config.update({
   region: REGION,
   endpoint: 'dynamodb.us-east-2.amazonaws.com',
-  // accessKeyId default can be used while using the downloadable version of DynamoDB. 
-  // For security reasons, do not store AWS Credentials in your files. Use Amazon Cognito instead.
   accessKeyId: ACCESS_ID,
-  // secretAccessKey default can be used while using the downloadable version of DynamoDB. 
-  // For security reasons, do not store AWS Credentials in your files. Use Amazon Cognito instead.
   secretAccessKey: ACCESS_KEY
 });
 
 var docClient = new AWS.DynamoDB.DocumentClient();
+var params = {
+    TableName: "KCM_Bus_Routes",
+    ProjectionExpression: "compkey, med_speed_m_s",
+};
+var segment_ary = [];
+var speed_ary = [];
 
-function scanData() {
-    document.getElementById('textarea').innerHTML += "Scanning table." + "\n";
+console.log("Starting scan");
+$("#statustext").text("Scanning..");
 
-    var params = {
-        TableName: "KCM_Bus_Routes",
-        ProjectionExpression: "compkey, med_speed_m_s",
-    };
+docClient.scan(params, onScan);
 
-    docClient.scan(params, onScan);
-
-    function onScan(err, data) {
-        if (err) {
-            document.getElementById('textarea').innerHTML += "Unable to scan the table: " + "\n" + JSON.stringify(err, undefined, 2);
-        } else {
-            // Print all the movies
-            document.getElementById('textarea').innerHTML += "Scan succeeded. " + "\n";
-            data.Items.forEach(function(segment) {
-                document.getElementById('textarea').innerHTML += segment.compkey + "\n";
-            });
-
-            // Continue scanning if we have more movies (per scan 1MB limitation)
-            document.getElementById('textarea').innerHTML += "Scanning for more..." + "\n";
+function onScan(err, data) {
+    if (err) {
+        console.log(err);
+    } else {
+        data.Items.forEach(function(segment) {
+            segment_ary.push(segment.compkey);
+            speed_ary.push(segment.med_speed_m_s);
+        });
+        if (typeof data.LastEvaluatedKey != "undefined") {
+            console.log("Scanning again..");
             params.ExclusiveStartKey = data.LastEvaluatedKey;
-            docClient.scan(params, onScan);            
+            docClient.scan(params, onScan);
+        } else {
+            console.log("Finished scanning");
+            console.log(segment_ary);
         }
     }
 }
